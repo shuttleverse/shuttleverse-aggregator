ec4043e8564a9f44bbc04f682604661e33a0f6e1
QW66666 <wusteven0829@gmail.com>
2025-03-05 18:47:42 -0500

    feat: added collection name and changed timestamp field type

--- src/main/java/com/shuttleverse/aggregator/model/ProductPriceHistory.java
+++ src/main/java/com/shuttleverse/aggregator/model/ProductPriceHistory.java
@@ -1,12 +1,15 @@
 package com.shuttleverse.aggregator.model;
 
-import java.time.LocalDateTime;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
 import java.util.Map;
 
+@Document(collection = "product_price_history")
 public interface ProductPriceHistory {
   String getProductId();
 
   Map<String, Double> getProductPrices();
 
-  LocalDateTime getTimestamp();
+  Date getTimestamp();
 }
