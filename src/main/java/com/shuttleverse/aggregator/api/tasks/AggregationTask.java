8901adf06d87d9138b0c3924db47e2cc8874ca8c
QW66666 <wusteven0829@gmail.com>
2025-03-16 16:39:39 -0400

    fix: added api key for owasp

--- src/main/java/com/shuttleverse/aggregator/api/tasks/AggregationTask.java
+++ src/main/java/com/shuttleverse/aggregator/api/tasks/AggregationTask.java
@@ -13,8 +13,6 @@ import com.shuttleverse.aggregator.service.VendorService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
